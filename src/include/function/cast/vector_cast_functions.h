92e8f3a62d4a2817e0419fc2494f60df36fdb541
Maxwell <49460053+mxwli@users.noreply.github.com>
2024-06-07 13:03:49 -0400

    add decimal functionality (#3580)
    logicaltype setup
    
    fix
    
    fix
    
    changes
    
    added more switch handling for decimal types
    
    more support
    
    compile fix
    
    function work
    
    stub
    
    work
    
    reconfigure cypher_lexer
    
    Run clang-format
    
    clang-tidy fix
    
    clang tidy
    
    Run clang-format
    
    clang-tidy
    
    add requested changes
    
    requested changes
    
    more requested changes
    
    fix
    
    Run clang-format
    
    add requested changes
    
    fustfmt
    
    update parser and lexer
    
    add prelim changes
    
    logicaltype setup
    
    fix
    
    fix
    
    changes
    
    added more switch handling for decimal types
    
    more support
    
    compile fix
    
    function work
    
    stub
    
    work
    
    reconfigure cypher_lexer
    
    Run clang-format
    
    clang-tidy fix
    
    clang tidy
    
    Run clang-format
    
    clang-tidy
    
    add requested changes
    
    requested changes
    
    more requested changes
    
    fix
    
    Run clang-format
    
    add requested changes
    
    fustfmt
    
    arithmetic functions
    
    progress
    
    work
    
    progress
    
    actually register the arithmetic functions...
    
    remove cast decimal
    
    progress
    
    finished(?)
    
    roundign
    
    progress
    
    now it nearly compiles!
    
    compiles
    
    add test
    
    test fix
    
    implicit cast addendum
    
    add decimal support for maxlogicaltype
    
    touch ups
    
    casting overflow check
    
    remove lasttest log
    
    add typename for using clauses
    
    fix floor/ceil
    
    clang tidy
    
    fix compile error
    
    Run clang-format
    
    clang-tidy
    
    fix tests
    
    cast tests
    
    fix binder error test
    
    Run clang-format
    
    add requested changes
    
    CI
    
    Run clang-format
    
    fix msvc build cI
    
    CI
    
    ci fix
    
    fix casting bug
    
    Run clang-format
    
    compiler fix
    
    Run clang-format
    
    compiler fix 2
    
    compiler issue fix?
    
    test
    
    test to see
    
    fix (hopefully)
    
    Run clang-format
    
    compiler fix
    
    oops
    
    add requested tests
    
    Run clang-format
    
    add requested changes 2
    
    CI fix
    
    remove mm-256kb
    
    fix bug
    
    requested changes
    
    clang tidy
    
    Run clang-format
    
    (cherry picked from commit 7859daacaebadf3fec5b6d934056b158a88791cc)

--- src/include/function/cast/vector_cast_functions.h
+++ src/include/function/cast/vector_cast_functions.h
@@ -18,7 +18,7 @@ struct CastFunction {
 
     template<typename EXECUTOR = UnaryFunctionExecutor>
     static std::unique_ptr<ScalarFunction> bindCastFunction(const std::string& functionName,
-        common::LogicalTypeID sourceTypeID, common::LogicalTypeID targetTypeID);
+        const common::LogicalType& sourceType, const common::LogicalType& targetType);
 };
 
 struct CastToDateFunction {
@@ -112,12 +112,6 @@ struct CastToFloatFunction {
     static constexpr const char* name = "TO_FLOAT";
 
     static constexpr const char* alias = "TOFLOAT";
-
-    static function_set getFunctionSet();
-};
-
-struct CastToDecimalFunction {
-    static constexpr const char* name = "TO_DECIMAL";
 
     static function_set getFunctionSet();
 };
