66647578d8b136bd6869c389c0224c92738ce402
wangqiang <qiang.wang01@transwarp.io>
2024-07-03 15:55:19 +0800

    DESC: sync kuzu master
    (cherry picked from commit 69558ca6414c8a3b4171deaba1b205abd4f1f8c9)

--- src/include/function/list/functions/list_slice_function.h
+++ src/include/function/list/functions/list_slice_function.h
@@ -8,7 +8,7 @@ namespace function {
 struct ListSlice {
     // Note: this function takes in a 1-based begin/end index (The index of the first value in the
     // listEntry is 1).
-    static inline void operation(common::list_entry_t& listEntry, int64_t& begin, int64_t& end,
+    static void operation(common::list_entry_t& listEntry, int64_t& begin, int64_t& end,
         common::list_entry_t& result, common::ValueVector& listVector,
         common::ValueVector& resultVector) {
         auto startIdx = begin;
@@ -19,29 +19,37 @@ struct ListSlice {
         auto srcPos = listEntry.offset + startIdx - 1;
         auto dstDataVector = common::ListVector::getDataVector(&resultVector);
         auto dstPos = result.offset;
-        for (auto i = 0u; i < endIdx - startIdx; i++) {
+        for (; startIdx < endIdx; startIdx++) {
             dstDataVector->copyFromVectorData(dstPos++, srcDataVector, srcPos++);
         }
     }
 
-    static inline void operation(common::ku_string_t& str, int64_t& begin, int64_t& end,
+    static void operation(common::ku_string_t& str, int64_t& begin, int64_t& end,
         common::ku_string_t& result, common::ValueVector& /*listValueVector*/,
         common::ValueVector& resultValueVector) {
         auto startIdx = begin;
         auto endIdx = end;
         normalizeIndices(startIdx, endIdx, str.len);
-        SubStr::operation(str, startIdx, std::min(endIdx - startIdx + 1, str.len - startIdx + 1),
-            result, resultValueVector);
+        SubStr::operation(str, startIdx, endIdx - startIdx + 1, result, resultValueVector);
     }
 
 private:
-    static inline void normalizeIndices(int64_t& startIdx, int64_t& endIdx, uint64_t size) {
+    static void normalizeIndices(int64_t& startIdx, int64_t& endIdx, uint64_t size) {
+        if (startIdx < 0) {
+            startIdx = size + startIdx + 1;
+        }
+        if (endIdx <= 0) {
+            endIdx = size + endIdx + 1;
+        }
+
         if (startIdx <= 0) {
             startIdx = 1;
         }
-        if (endIdx <= 0 || (uint64_t)endIdx > size) {
+
+        if ((uint64_t)endIdx > size) {
             endIdx = size + 1;
         }
+
         if (startIdx > endIdx) {
             endIdx = startIdx;
         }
@@ -66,9 +74,6 @@ struct ListTail {
 
 private:
     static inline void normalizeIndices(int64_t& startIdx, int64_t& endIdx, uint64_t size) {
-        if (startIdx <= 0) {
-            startIdx = 1;
-        }
         if (endIdx <= 0 || (uint64_t)endIdx > size) {
             endIdx = size + 1;
         }
