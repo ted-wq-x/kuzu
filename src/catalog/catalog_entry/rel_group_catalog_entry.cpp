antonymarion/ladybug

Rework iterateCatalogEntries and catalog entry oid (#4057)
* Use oid across catalog entry

* Run clang-format

* bump extension version

---------

Co-authored-by: CI Bot <andyfengHKU@users.noreply.github.com>

src/catalog/catalog_entry/rel_group_catalog_entry.cpp
@@ -16,24 +16,27 @@ namespace kuzu {
 namespace catalog {
 
 RelGroupCatalogEntry::RelGroupCatalogEntry(CatalogSet* set, std::string tableName,
-    common::table_id_t tableID, std::vector<common::table_id_t> relTableIDs)
-    : TableCatalogEntry{set, CatalogEntryType::REL_GROUP_ENTRY, std::move(tableName), tableID},
+    std::vector<table_id_t> relTableIDs)
+    : TableCatalogEntry{set, CatalogEntryType::REL_GROUP_ENTRY, std::move(tableName)},
       relTableIDs{std::move(relTableIDs)} {}
 
-bool RelGroupCatalogEntry::isParent(common::table_id_t childID) {
-    auto it = find_if(relTableIDs.begin(), relTableIDs.end(),
-        [&](common::table_id_t relTableID) { return relTableID == childID; });
+bool RelGroupCatalogEntry::isParent(table_id_t childID) {
+    const auto it = find_if(relTableIDs.begin(), relTableIDs.end(),
+        [&](table_id_t relTableID) { return relTableID == childID; });
     return it != relTableIDs.end();
 }
 
-void RelGroupCatalogEntry::serialize(common::Serializer& serializer) const {
+void RelGroupCatalogEntry::serialize(Serializer& serializer) const {
     TableCatalogEntry::serialize(serializer);
+    serializer.writeDebuggingInfo("relTableIDs");
     serializer.serializeVector(relTableIDs);
 }
 
 std::unique_ptr<RelGroupCatalogEntry> RelGroupCatalogEntry::deserialize(
-    common::Deserializer& deserializer) {
-    std::vector<common::table_id_t> relTableIDs;
+    Deserializer& deserializer) {
+    std::string debuggingInfo;
+    std::vector<table_id_t> relTableIDs;
+    deserializer.validateDebuggingInfo(debuggingInfo, "relTableIDs");
     deserializer.deserializeVector(relTableIDs);
     auto relGroupEntry = std::make_unique<RelGroupCatalogEntry>();
     relGroupEntry->relTableIDs = std::move(relTableIDs);
@@ -48,10 +51,9 @@ std::unique_ptr<TableCatalogEntry> RelGroupCatalogEntry::copy() const {
 }
 
 static std::optional<binder::BoundCreateTableInfo> getBoundCreateTableInfoForTable(
-    transaction::Transaction* transaction, const CatalogEntrySet& entries,
-    common::table_id_t tableID) {
+    transaction::Transaction* transaction, const CatalogEntrySet& entries, table_id_t tableID) {
     for (auto& [name, entry] : entries) {
-        auto current = common::ku_dynamic_cast<CatalogEntry*, TableCatalogEntry*>(entry);
+        auto current = ku_dynamic_cast<CatalogEntry*, TableCatalogEntry*>(entry);
         if (current->getTableID() == tableID) {
             auto boundInfo = current->getBoundCreateTableInfo(transaction);
             return boundInfo;

src/include/catalog/catalog_set.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <mutex>
 
 #include "catalog_entry/catalog_entry.h"
@@ -25,51 +26,50 @@ class CatalogSet {
     friend class storage::UndoBuffer;
 
 public:
-    //===--------------------------------------------------------------------===//
-    // getters & setters
-    //===--------------------------------------------------------------------===//
-    bool containsEntry(transaction::Transaction* transaction, const std::string& name);
-    CatalogEntry* getEntry(transaction::Transaction* transaction, const std::string& name);
-    KUZU_API void createEntry(transaction::Transaction* transaction,
+    bool containsEntry(const transaction::Transaction* transaction, const std::string& name);
+    CatalogEntry* getEntry(const transaction::Transaction* transaction, const std::string& name);
+    KUZU_API common::oid_t createEntry(transaction::Transaction* transaction,
         std::unique_ptr<CatalogEntry> entry);
-    void dropEntry(transaction::Transaction* transaction, const std::string& name);
+    void dropEntry(transaction::Transaction* transaction, const std::string& name,
+        common::oid_t oid);
     void alterEntry(transaction::Transaction* transaction, const binder::BoundAlterInfo& alterInfo);
-    CatalogEntrySet getEntries(transaction::Transaction* transaction);
 
-    common::idx_t assignNextOID() {
-        std::lock_guard lck{mtx};
-        return nextOID++;
-    }
+    CatalogEntrySet getEntries(const transaction::Transaction* transaction);
+    void iterateEntriesOfType(const transaction::Transaction* transaction, CatalogEntryType type,
+        const std::function<void(CatalogEntry*)>& func);
+    CatalogEntry* getEntryOfOID(const transaction::Transaction* transaction, common::oid_t oid);
 
-    //===--------------------------------------------------------------------===//
-    // serialization & deserialization
-    //===--------------------------------------------------------------------===//
     void serialize(common::Serializer serializer) const;
     static std::unique_ptr<CatalogSet> deserialize(common::Deserializer& deserializer);
 
 private:
-    bool containsEntryNoLock(transaction::Transaction* transaction, const std::string& name) const;
-    CatalogEntry* getEntryNoLock(transaction::Transaction* transaction, const std::string& name);
-    CatalogEntry* createEntryNoLock(transaction::Transaction* transaction,
+    bool containsEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    CatalogEntry* getEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    CatalogEntry* createEntryNoLock(const transaction::Transaction* transaction,
         std::unique_ptr<CatalogEntry> entry);
-    CatalogEntry* dropEntryNoLock(transaction::Transaction* transaction, const std::string& name);
+    CatalogEntry* dropEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name, common::oid_t oid);
 
-    void validateExistNoLock(transaction::Transaction* transaction, const std::string& name) const;
-    void validateNotExistNoLock(transaction::Transaction* transaction,
+    void validateExistNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    void validateNotExistNoLock(const transaction::Transaction* transaction,
         const std::string& name) const;
 
     void emplaceNoLock(std::unique_ptr<CatalogEntry> entry);
     void eraseNoLock(const std::string& name);
 
-    std::unique_ptr<CatalogEntry> createDummyEntryNoLock(std::string name) const;
+    static std::unique_ptr<CatalogEntry> createDummyEntryNoLock(std::string name,
+        common::oid_t oid);
 
-    CatalogEntry* traverseVersionChainsForTransactionNoLock(transaction::Transaction* transaction,
-        CatalogEntry* currentEntry) const;
-    CatalogEntry* getCommittedEntryNoLock(CatalogEntry* entry) const;
+    static CatalogEntry* traverseVersionChainsForTransactionNoLock(
+        const transaction::Transaction* transaction, CatalogEntry* currentEntry);
+    static CatalogEntry* getCommittedEntryNoLock(CatalogEntry* entry);
 
 private:
     std::mutex mtx;
-    common::idx_t nextOID = 0;
+    common::oid_t nextOID = 0;
     common::case_insensitive_map_t<std::unique_ptr<CatalogEntry>> entries;
 };
 

ted-wq-x/kuzu

(cherry picked from commit 9a97f751f564af17cf0a5d53b9155364f8efc587)

src/catalog/catalog_entry/rel_group_catalog_entry.cpp
@@ -16,24 +16,27 @@ namespace kuzu {
 namespace catalog {
 
 RelGroupCatalogEntry::RelGroupCatalogEntry(CatalogSet* set, std::string tableName,
-    common::table_id_t tableID, std::vector<common::table_id_t> relTableIDs)
-    : TableCatalogEntry{set, CatalogEntryType::REL_GROUP_ENTRY, std::move(tableName), tableID},
+    std::vector<table_id_t> relTableIDs)
+    : TableCatalogEntry{set, CatalogEntryType::REL_GROUP_ENTRY, std::move(tableName)},
       relTableIDs{std::move(relTableIDs)} {}
 
-bool RelGroupCatalogEntry::isParent(common::table_id_t childID) {
-    auto it = find_if(relTableIDs.begin(), relTableIDs.end(),
-        [&](common::table_id_t relTableID) { return relTableID == childID; });
+bool RelGroupCatalogEntry::isParent(table_id_t childID) {
+    const auto it = find_if(relTableIDs.begin(), relTableIDs.end(),
+        [&](table_id_t relTableID) { return relTableID == childID; });
     return it != relTableIDs.end();
 }
 
-void RelGroupCatalogEntry::serialize(common::Serializer& serializer) const {
+void RelGroupCatalogEntry::serialize(Serializer& serializer) const {
     TableCatalogEntry::serialize(serializer);
+    serializer.writeDebuggingInfo("relTableIDs");
     serializer.serializeVector(relTableIDs);
 }
 
 std::unique_ptr<RelGroupCatalogEntry> RelGroupCatalogEntry::deserialize(
-    common::Deserializer& deserializer) {
-    std::vector<common::table_id_t> relTableIDs;
+    Deserializer& deserializer) {
+    std::string debuggingInfo;
+    std::vector<table_id_t> relTableIDs;
+    deserializer.validateDebuggingInfo(debuggingInfo, "relTableIDs");
     deserializer.deserializeVector(relTableIDs);
     auto relGroupEntry = std::make_unique<RelGroupCatalogEntry>();
     relGroupEntry->relTableIDs = std::move(relTableIDs);
@@ -48,9 +51,9 @@ std::unique_ptr<TableCatalogEntry> RelGroupCatalogEntry::copy() const {
 }
 
 static std::optional<binder::BoundCreateTableInfo> getBoundCreateTableInfoForTable(
-    transaction::Transaction* transaction, const CatalogEntryVector& entries, common::table_id_t tableID) {
+    transaction::Transaction* transaction, const CatalogEntryVector& entries, table_id_t tableID) {
     for (auto& entry : entries) {
-        auto current = common::ku_dynamic_cast<CatalogEntry*, TableCatalogEntry*>(entry);
+        auto current = ku_dynamic_cast<CatalogEntry*, TableCatalogEntry*>(entry);
         if (current->getTableID() == tableID) {
             auto boundInfo = current->getBoundCreateTableInfo(transaction);
             return boundInfo;

src/include/catalog/catalog_set.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <mutex>
 
 #include "catalog_entry/catalog_entry.h"
@@ -25,51 +26,50 @@ class CatalogSet {
     friend class storage::UndoBuffer;
 
 public:
-    //===--------------------------------------------------------------------===//
-    // getters & setters
-    //===--------------------------------------------------------------------===//
-    bool containsEntry(transaction::Transaction* transaction, const std::string& name);
-    CatalogEntry* getEntry(transaction::Transaction* transaction, const std::string& name);
-    KUZU_API void createEntry(transaction::Transaction* transaction,
+    bool containsEntry(const transaction::Transaction* transaction, const std::string& name);
+    CatalogEntry* getEntry(const transaction::Transaction* transaction, const std::string& name);
+    KUZU_API common::oid_t createEntry(transaction::Transaction* transaction,
         std::unique_ptr<CatalogEntry> entry);
-    void dropEntry(transaction::Transaction* transaction, const std::string& name);
+    void dropEntry(transaction::Transaction* transaction, const std::string& name,
+        common::oid_t oid);
     void alterEntry(transaction::Transaction* transaction, const binder::BoundAlterInfo& alterInfo);
-    CatalogEntryVector getEntries(transaction::Transaction* transaction);
 
-    common::idx_t assignNextOID() {
-        std::lock_guard lck{mtx};
-        return nextOID++;
-    }
+    CatalogEntryVector getEntries(const transaction::Transaction* transaction);
+    void iterateEntriesOfType(const transaction::Transaction* transaction, CatalogEntryType type,
+        const std::function<void(CatalogEntry*)>& func);
+    CatalogEntry* getEntryOfOID(const transaction::Transaction* transaction, common::oid_t oid);
 
-    //===--------------------------------------------------------------------===//
-    // serialization & deserialization
-    //===--------------------------------------------------------------------===//
     void serialize(common::Serializer serializer) const;
     static std::unique_ptr<CatalogSet> deserialize(common::Deserializer& deserializer);
 
 private:
-    bool containsEntryNoLock(transaction::Transaction* transaction, const std::string& name) const;
-    CatalogEntry* getEntryNoLock(transaction::Transaction* transaction, const std::string& name);
-    CatalogEntry* createEntryNoLock(transaction::Transaction* transaction,
+    bool containsEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    CatalogEntry* getEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    CatalogEntry* createEntryNoLock(const transaction::Transaction* transaction,
         std::unique_ptr<CatalogEntry> entry);
-    CatalogEntry* dropEntryNoLock(transaction::Transaction* transaction, const std::string& name);
+    CatalogEntry* dropEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name, common::oid_t oid);
 
-    void validateExistNoLock(transaction::Transaction* transaction, const std::string& name) const;
-    void validateNotExistNoLock(transaction::Transaction* transaction,
+    void validateExistNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    void validateNotExistNoLock(const transaction::Transaction* transaction,
         const std::string& name) const;
 
     void emplaceNoLock(std::unique_ptr<CatalogEntry> entry);
     void eraseNoLock(const std::string& name);
 
-    std::unique_ptr<CatalogEntry> createDummyEntryNoLock(std::string name) const;
+    static std::unique_ptr<CatalogEntry> createDummyEntryNoLock(std::string name,
+        common::oid_t oid);
 
-    CatalogEntry* traverseVersionChainsForTransactionNoLock(transaction::Transaction* transaction,
-        CatalogEntry* currentEntry) const;
-    CatalogEntry* getCommittedEntryNoLock(CatalogEntry* entry) const;
+    static CatalogEntry* traverseVersionChainsForTransactionNoLock(
+        const transaction::Transaction* transaction, CatalogEntry* currentEntry);
+    static CatalogEntry* getCommittedEntryNoLock(CatalogEntry* entry);
 
 private:
     std::mutex mtx;
-    common::idx_t nextOID = 0;
+    common::oid_t nextOID = 0;
     common::case_insensitive_map_t<std::unique_ptr<CatalogEntry>> entries;
 };
 